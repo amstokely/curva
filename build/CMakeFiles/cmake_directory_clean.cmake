55909c22979e3f28b9c8b50f3354b9b39ef69a8e
astokely <astokely1994@pointloma.edu>
2022-11-17 10:26:45 -0700

    Removed build directory

--- build/CMakeFiles/cmake_directory_clean.cmake
+++
@@ -1,5 +0,0 @@
-file(REMOVE_RECURSE
-  "/home/andy/CLionProjects/curva/lib/pycurvaPYTHON_wrap.cxx"
-  "/home/andy/CLionProjects/curva/lib/pycurva.py"
-  "CMakeFiles/pycurva.dir/pycurvaPYTHON.stamp"
-)
